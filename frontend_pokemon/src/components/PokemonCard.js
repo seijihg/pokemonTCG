11b490b3abb3c819c534f4c71880bf686995b5ac
Danielle <dmiz@brandeis.edu>
2019-08-19 17:44:34 +0100

    fixed css

--- frontend_pokemon/src/components/PokemonCard.js
+++ frontend_pokemon/src/components/PokemonCard.js
@@ -1,22 +1,24 @@
-import React from 'react';
+import React from "react";
 
 function PokemonCard(props) {
-  const {name, imageUrl, hp, attacks} = props.pokemon
+  const { name, imageUrl, hp, attacks } = props.pokemon;
 
   const skillSet = () => {
-    return attacks.map(elem =>{
+    return attacks.map(elem => {
       return (
-        <li>{elem.name}: {elem.damage}</li>
-      )
-    })
-  }
-    return (
-      <div className="card">
-        <div>
-          <img src={imageUrl} width="180px"></img>
-        </div>
+        <li>
+          {elem.name}: {elem.damage}
+        </li>
+      );
+    });
+  };
+  return (
+    <div className="card" onClick={props.onCardClick}>
+      <div>
+        <img src={imageUrl} width="180px" />
       </div>
-    );
-  }
-  
-  export default PokemonCard;
+    </div>
+  );
+}
+
+export default PokemonCard;
